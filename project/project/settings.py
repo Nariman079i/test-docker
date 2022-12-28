c9b2b8ac9a12ff955b60316bac6eba807ef03f87
Nariman <Nariman079i>
2022-12-28 13:11:31 +0300

    edit cors

--- project/project/settings.py
+++ project/project/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -78,6 +77,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 CORS_ALLOWED_ORIGINS = []
+CORS_ORIGIN_ALLOW_ALL = True
 AUTH_USER_MODEL = 'users.DefaultUser'
 
 # Database
